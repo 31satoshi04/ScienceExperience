cee64c3a994140e4b93a6eefa1299c100a62b172
31satoshi04 <g1772046@tcu.ac.jp>
2019-07-06 01:38:48 +0900

    init firebase

--- automusic/js/script.js
+++ automusic/js/script.js
@@ -1,3 +1,17 @@
+  // Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAdpxD0cZBJZbjxnCunxj8iouou-muTC1c",
+    authDomain: "otanilab-music-downloader.firebaseapp.com",
+    databaseURL: "https://otanilab-music-downloader.firebaseio.com",
+    projectId: "otanilab-music-downloader",
+    storageBucket: "otanilab-music-downloader.appspot.com",
+    messagingSenderId: "238815551872",
+    appId: "1:238815551872:web:1c00f909a1622823"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
 const decision_btn = document.getElementById("decision-btn");
 
 decision_btn.addEventListener("click", function () {
@@ -15,3 +29,18 @@ decision_btn.addEventListener("click", function () {
   const parent = modal_window.parentNode;
   parent.removeChild(modal_window);
 });
+
+
+var docRef = db.collection("seat0").doc("1");
+
+docRef.get().then(function(doc) {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch(function(error) {
+    console.log("Error getting document:", error);
+});
+
