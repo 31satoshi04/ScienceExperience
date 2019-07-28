c67d7d6111162a1d29a80586e9e5e70f87bf5448
hiro <mh.ikura@gmail.com>
2019-07-28 12:37:13 +0900

    mp3 dowload時にファイルが破損しているバグ修正

--- automusic/js/script.js
+++ automusic/js/script.js
@@ -3,59 +3,77 @@
 let mp3_url;
 
 const decision_btn = document.getElementById("decision-btn");
-decision_btn.addEventListener("click", async function () {
+decision_btn.addEventListener("click", async function() {
   const modal_window = document.getElementById("modal");
 
   // 授業回数及び座席番号取得
-  const class_time = document.querySelector('.class-number select').value;
-  const seat_number = document.querySelector('.seat-number select').value;
+  const class_time = document.querySelector(".class-number select").value;
+  const seat_number = document.querySelector(".seat-number select").value;
 
   // DBから、作成した楽曲情報を取得
-  const created_music_info = await db.collection(`work${class_time}`).doc(`seat${seat_number}`).get().then(function (snapshot) {
-    return new Promise((resolve, reject) => {
-      if (snapshot.exists) {
-        resolve(snapshot.data());
-      } else {
-        // 該当パスに値がない時
-        console.error(`データがありません: ${snapshot.data()}`);
-        reject;
+  const created_music_info = await db
+    .collection(`work${class_time}`)
+    .doc(`seat${seat_number}`)
+    .get()
+    .then(
+      function(snapshot) {
+        return new Promise((resolve, reject) => {
+          if (snapshot.exists) {
+            resolve(snapshot.data());
+          } else {
+            // 該当パスに値がない時
+            console.error(`データがありません: ${snapshot.data()}`);
+            reject;
+          }
+        });
+      },
+      function(error) {
+        // DBへのアクセス権限がないとき or 無効クエリの時
+        console.error(`Error: getting document:${error}`);
       }
-    });
-  }, function (error) {
-    // DBへのアクセス権限がないとき or 無効クエリの時
-    console.error(`Error: getting document:${error}`);
-  });
+    );
 
   // DOM作成
-  let music_list_DOM = '';
-  Object.values(created_music_info)[0].forEach(async function (card_info) {
+  let music_list_DOM = "";
+  Object.values(created_music_info)[0].forEach(async function(card_info) {
     const song_tone = card_info.song_tone;
     const song_id = card_info.song_id;
-    storage.ref(`MP3/work${class_time}/seat${seat_number}/${song_id}.mp3`).getDownloadURL().then(function (url) {
-      mp3_url = url;
-      music_list_DOM = createMuscListDOM(card_info, mp3_url);
-    }).then(function () {
-      const ul = document.querySelector(`#Top ul`)
-      const cards = document.createElement('li');
+    const song_name = card_info.song_name;
+    storage
+      .ref(`MP3/work${class_time}/seat${seat_number}/${song_id}.mp3`)
+      .getDownloadURL()
+      .then(function(url) {
+        mp3_url = url;
+        music_list_DOM = createMuscListDOM(card_info, mp3_url);
 
-      cards.classList.add('card');
-      cards.classList.add(song_tone);
-      cards.innerHTML = music_list_DOM;
-      ul.appendChild(cards);
-      const mp3_download_btn = document.getElementById("download-music-btn");
-      mp3_download_btn.addEventListener("click", function (event) {
-        event.preventDefault();
-        var blob = new Blob([mp3_url], {type: "audio/mp3"});
-        saveAs(blob, 'test.mp3');
-        console.log("Downloadなう");
+        const ul = document.querySelector(`#Top ul`);
+        const cards = document.createElement("li");
+
+        cards.classList.add("card");
+        cards.classList.add(song_tone);
+        cards.innerHTML = music_list_DOM;
+        ul.appendChild(cards);
+        const mp3_download_btn = document.getElementById("download-music-btn");
+        mp3_download_btn.addEventListener("click", function(event) {
+          event.preventDefault();
+
+          const xhr = new XMLHttpRequest();
+          xhr.responseType = "blob";
+          xhr.onload = function(event) {
+            const blob = xhr.response;
+            saveAs(blob, song_name);
+          };
+          xhr.open("GET", url);
+          xhr.send();
+
+          console.log("Downloadなう");
+        });
       });
-    });
-  })
+  });
 
   // モーダルウィンドウを取り除く
   modal_window.parentNode.removeChild(modal_window);
 });
-
 
 // 関数
 // =========================================================
@@ -82,9 +100,9 @@ function createMuscListDOM(music_info, mp3_url) {
         <div class="music-info">
           <p>作曲に使った曲</p>
           <ul class="used-music-list">`;
-  for (let used_song of used_song_list) card_elements += `<li>${used_song}</li>`;
-  card_elements +=
-    `</ul>
+  for (let used_song of used_song_list)
+    card_elements += `<li>${used_song}</li>`;
+  card_elements += `</ul>
         </div>
         <div class="downloader">
           <a id="download-PDF-btn">
